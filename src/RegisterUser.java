7834229a5f4ef244cc522bdee1b1d8494ca5286d
klarrrr <klarenzcobie99@gmail.com>
2024-06-08 19:51:10 +0800

    Sya paren
    sana tlaga haysss

---
+++ src/RegisterUser.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RegisterUser
+{
+    HashMap<String, User> myMap = new HashMap<>();
+
+    public void register()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String username, password, answer;
+
+        while(true)
+        {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            System.out.println("======================================");
+
+            System.out.print("Create Username: ");
+            username = sc.nextLine();
+
+            System.out.print("Create Password: ");
+            password = sc.nextLine();
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            registerUser(new User(username, password));
+
+            System.out.println("======================================");
+
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+            System.out.print("Add This User?(y/N): ");
+            answer = sc.nextLine();
+
+            if (answer.equals("Y") || answer.equals("y"))
+            {
+                break;
+            }
+        }
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("======================================");
+
+        System.out.println("Account Succesfully Created!");
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+        sc.close();
+    }
+
+    private void registerUser(User username)
+    {
+        myMap.put(username.getUsername(), username);
+    }
+
+    private User getUser(String username)
+    {
+        return myMap.get(username);
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,55 @@
+public class User 
+{
+    String username, password;
+    int balance;
+
+    public User(String username, String password, int balance)
+    {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public void setBalance(int balance)
+    {
+        this.balance = balance;
+    }
+    
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public int getBalance()
+    {
+        return balance;
+    }
+
+    public void print()
+    {
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("Balance: " + Integer.toString(balance));
+    }
+}
